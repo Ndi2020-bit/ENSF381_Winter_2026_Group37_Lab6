61d0e6863412aa080d29ccd83ac354e7fb97980a
Nnamdi Onaga <onaga.nnamdi@gmail.com>
2026-03-13 16:37:20 -0600

    Finished Ex 1

--- lab06/Starter/src/exercise1/UserDirectoryPage.js
+++ lab06/Starter/src/exercise1/UserDirectoryPage.js
@@ -1,25 +1,46 @@
+import { useState, useEffect } from 'react';
 import Controls from './Controls';
-import sampleUsers from './sampleUsers';
+//import sampleUsers from './sampleUsers';
 import UserList from './UserList';
 
 function UserDirectoryPage() {
   // TODO: add users, sortBy, and viewMode state in this component.
+  const [users, setUsers] = useState([]);
+  const [sortBy, setSortBy] = useState('id');
+  const [viewMode, setViewMode] = useState('grid');
   // TODO: fetch the initial users with useEffect.
+  useEffect(() => {
+    fetch('https://69a1edcc2e82ee536fa29f3f.mockapi.io/users_api')
+      .then(response => response.json())
+      .then(data => setUsers(data))
+      .catch(err => console.error("Failed to fetch users", err));
+  }, []);
 
   function handleDeleteClick(userId) {
-    console.log('TODO: delete the user with id', userId);
+    fetch(`https://69a1edcc2e82ee536fa29f3f.mockapi.io/users_api/${userId}`, {
+      method: 'DELETE',
+    })
+      .then(() => {
+        // Filter out the deleted user from state
+        setUsers(prevUsers => prevUsers.filter(user => user.id !== userId));
+      })
+      .catch(err => console.error("Failed to delete user", err));
   }
 
   function handleSortByGroupClick() {
-    console.log('TODO: sort users by user_group');
+    const sorted = [...users].sort((a, b) => a.user_group - b.user_group);
+    setUsers(sorted);
+    setSortBy('group');
   }
 
   function handleSortByIdClick() {
-    console.log('TODO: sort users by id');
+    const sorted = [...users].sort((a, b) => a.id - b.id);
+    setUsers(sorted);
+    setSortBy('id');
   }
 
   function handleViewToggleClick() {
-    console.log('TODO: switch between grid and list layouts');
+    setViewMode(prev => prev === 'grid' ? 'list' : 'grid');
   }
 
   return (
@@ -30,12 +51,15 @@ function UserDirectoryPage() {
 
       <section className="panel">
         <h2>Controls</h2>
-        <Controls />
+        <Controls onSortByIdClick={handleSortByIdClick}
+          onSortByGroupClick={handleSortByGroupClick}
+          onViewToggleClick={handleViewToggleClick}
+          onDeleteClick={handleDeleteClick} />
       </section>
 
       <section className="panel">
         <h2>All Users</h2>
-        <UserList users={sampleUsers} viewMode="grid" />
+        <UserList users={users} viewMode={viewMode} />
       </section>
     </>
   );
